8570805a200f6b3a6f1293e9df5e8aec91383691
hazthegoat <hazaraskari@gmail.com>
2021-07-01 19:49:14 +0200

    removing strapi

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '7977aa8ca0c5f2cc8aaa1d7a999e270c'),
-    },
-  },
-});
